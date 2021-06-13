2233e1d65fec11955fdd9e5b679314245c5a242b
typicallhavok <53092887+typicallhavok@users.noreply.github.com>
2021-06-13 23:01:46 +0530

    gh

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 2323;
-app.get('/', (req, res) => res.send('Harmony is Alive!'));
-
-app.listen(port, () => console.log(`Harmony is listening to http://localhost:${port}`));
